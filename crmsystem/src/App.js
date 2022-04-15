ac69bd6b20c3870857b6bca74a5cef688eece6d5
osama abualhayjaa <38279798+osama151@users.noreply.github.com>
2022-04-15 23:59:14 +0300

    routs

--- crmsystem/src/App.js
+++ crmsystem/src/App.js
@@ -1,11 +1,24 @@
-import Home from "./pages/home/Home.jsx"
-import Login from "./pages/login/Login.jsx";
+import Home from "./pages/home/Home"
+import Login from "./pages/login/Login";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-   < Home />
-   <Login />
+   <BrowserRouter>
+    <Routes>
+      <Route path="/"  >
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+
+
+         </Route>
+    </Routes>
+  </BrowserRouter>,
     </div>
   );
 }
--- crmsystem/src/pages/home/Home.jsx
+++ crmsystem/src/pages/home/Home.jsx
@@ -3,9 +3,9 @@ import "./home.scss"
 export default function Home() {
   return (
     <div className="home">
-        <h1 className="title">
+       
             home
-        </h1>
+       
     </div>
   )
 }
