57a8a4f55ca90b8527cefeb021ebe1c313688adf
osama abualhayjaa <38279798+osama151@users.noreply.github.com>
2022-04-15 23:10:06 +0300

    Revert "start2"
    This reverts commit 994a237d707f945195bc25b88bfbff9efc720e1f.

--- crmsystem/src/App.js
+++ crmsystem/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Home from "./home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          gersgrdgdrgdrgdrgdrgdr
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn osamaaaaaaaaaaaaaa
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ crmsystem/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div className="home">
+
+<Navbar />
+        </div>
+
+
+);
+};
+
+export default Home;
