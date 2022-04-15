c2faf970691e9736374bed0b8d69edaf38205b32
osama abualhayjaa <38279798+osama151@users.noreply.github.com>
2022-04-16 01:24:21 +0300

    sidebar

--- crmsystem/src/pages/home/Home.jsx
+++ crmsystem/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.scss"
 
 export default function Home() {
   return (
     <div className="home">
-       
-            home
+        <Sidebar />
+      <div className="homeContiner">container</div>
        
     </div>
   )
