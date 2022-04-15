e873d176e8e4588904a48e775dd48bcc7cbbdc29
osama abualhayjaa <38279798+osama151@users.noreply.github.com>
2022-04-15 23:25:49 +0300

    login

--- crmsystem/src/App.js
+++ crmsystem/src/App.js
@@ -1,9 +1,11 @@
 import Home from "./pages/home/Home.jsx"
+import Login from "./pages/login/Login.jsx";
 
 function App() {
   return (
     <div className="App">
    < Home />
+   <Login />
     </div>
   );
 }
