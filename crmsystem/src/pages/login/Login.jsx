547f612beca174d34337bcf1ceca157973f31b87
hasan alshater <49233388+hasanalsahter@users.noreply.github.com>
2022-04-15 23:57:35 +0300

    login form

--- crmsystem/src/pages/login/Login.jsx
+++ crmsystem/src/pages/login/Login.jsx
@@ -2,6 +2,15 @@ import "./login.scss"
 
 export default function Login() {
   return (
-    <div className="login">Login</div>
+
+    <div className="login">
+      <form>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+
+      </form>
+
+    </div>
   )
 }
