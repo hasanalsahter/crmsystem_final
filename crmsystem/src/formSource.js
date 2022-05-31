a726044bb4f4d3ba211b6a535ced8db17b498d2c
osama abualhayjaa <38279798+osama151@users.noreply.github.com>
2022-05-31 14:35:33 +0300

    Revert "newuser"1
    This reverts commit 8ce9e16aeb7fc0e28c8bc1409a53ab480c9d8cd9.

---
+++ crmsystem/src/formSource.js
@@ -0,0 +1,21 @@
+export const userInputs=[
+    {
+       id:1,
+       label:"Username",
+       type:"text",
+       placeholder:"john_doe",
+    },
+    {
+       id:2,
+       label:"Name and surname",
+       type:"text",
+       placeholder:"John Doe",
+     },
+    {
+        id:3,
+        label:"Email",
+        type:"mail",
+        placeholder:"john_doe@gmail.com",
+    },
+    
+];
